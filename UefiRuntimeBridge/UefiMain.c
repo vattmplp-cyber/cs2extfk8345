566d391bb53a6308db525e28c4cd94cbf0d85f2d
vattmplp-cyber <vattmplp@gmail.com>
2026-09-09 19:55:39 -0500

    Update UefiMain.c

--- UefiRuntimeBridge/UefiMain.c
+++ UefiRuntimeBridge/UefiMain.c
@@ -3,7 +3,6 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
 #include <Library/BaseMemoryLib.h>
-#include <Library/MemoryAllocationLib.h>
 
 // НАШ УНІКАЛЬНИЙ GUID
 #define COMPILER_UEFI_GUID \
@@ -11,6 +10,7 @@
 
 static EFI_GUID gMyDriverGuid = COMPILER_UEFI_GUID;
 static EFI_SET_VARIABLE gOriginalSetVariable = NULL;
+static EFI_EVENT gVirtualAddressMapEvent = NULL;
 
 typedef struct {
     UINT32     command_id;      
@@ -20,7 +20,7 @@ typedef struct {
     UINT64     read_size;
 } UEFI_READ_PACKET;
 
-// --- ВИПРАВЛЕНИЙ БЕЗПЕЧНИЙ ХУК-ПЕРЕХОПЛЮВАЧ ---
+// --- БЕЗПЕЧНИЙ ХУК З СИНХРОНІЗАЦІЄЮ АДРЕС ---
 EFI_STATUS
 EFIAPI
 MyCustomSetVariable (
@@ -31,19 +31,15 @@ MyCustomSetVariable (
   IN  VOID      *Data
   )
 {
-    // Золоте правило безпеки: якщо Windows прислала порожні системні параметри,
-    // ми їх не чіпаємо і миттєво віддаємо оригінальному BIOS!
     if (VariableName == NULL || VendorGuid == NULL) {
         return gOriginalSetVariable(VariableName, VendorGuid, Attributes, DataSize, Data);
     }
 
-    // Тепер порівняння GUID та імені змінної є повністю безпечним
     if (CompareGuid(VendorGuid, &gMyDriverGuid) && StrCmp(VariableName, L"UefiRead") == 0) {
         if (Data != NULL && DataSize >= sizeof(UEFI_READ_PACKET)) {
             UEFI_READ_PACKET *packet = (UEFI_READ_PACKET*)Data;
 
             if (packet->command_id == 1) { 
-                // Безпечне копіювання: перевіряємо адреси перед виконанням транзиту
                 if (packet->output_buffer != 0 && packet->source_address != 0 && packet->read_size > 0) {
                     CopyMem((VOID*)(packet->output_buffer), (VOID*)(packet->source_address), (UINTN)packet->read_size);
                     return EFI_SUCCESS;
@@ -52,8 +48,23 @@ MyCustomSetVariable (
         }
     }
 
-    // Передаємо всі звичайні системні запити оригінальній функції плати Acer
     return gOriginalSetVariable(VariableName, VendorGuid, Attributes, DataSize, Data);
+}
+
+// --- ФУНКЦІЯ ПЕРЕХОДУ НА ВІРТУАЛЬНІ АДРЕСИ ВІНДОВС (СЕКРЕТ СТАБІЛЬНОСТІ) ---
+VOID
+EFIAPI
+OnSetVirtualAddressMap (
+  IN EFI_EVENT  Event,
+  IN VOID       *Context
+  )
+{
+    // Коли Windows змінює карту пам'яті, ми конвертуємо адресу нашої оригінальної функції BIOS,
+    // щоб хук знав, куди передавати звичайні запити операційної системи!
+    if (gRT != NULL && gOriginalSetVariable != NULL) {
+        gRT->ConvertPointer(0, (VOID**)&gOriginalSetVariable);
+        gRT->ConvertPointer(0, (VOID**)&gRT);
+    }
 }
 
 // --- ТОЧКА ВХОДУ UEFI ДРАЙВЕРА ---
@@ -64,13 +75,18 @@ UefiMain (
   IN EFI_SYSTEM_TABLE  *SystemTable
   )
 {
-    // Перевіряємо, щоб не зробити подвійний хук, якщо ми перезавантажуємо драйвер у Shell
-    if (gRT->SetVariable == MyCustomSetVariable) {
-        return EFI_SUCCESS;
-    }
-
     gOriginalSetVariable = gRT->SetVariable;
     gRT->SetVariable = MyCustomSetVariable;
+
+    // Реєструємо подію переходу: коли Windows викличе SetVirtualAddressMap, 
+    // наш драйвер автоматично оновить свої покажчики в пам'яті й захистить систему від БСОДу!
+    gBS->CreateEvent (
+           EVT_SIGNAL_VIRTUAL_ADDRESS_MAP,
+           TPL_NOTIFY,
+           OnSetVirtualAddressMap,
+           NULL,
+           &gVirtualAddressMapEvent
+           );
 
     return EFI_SUCCESS;
 }
