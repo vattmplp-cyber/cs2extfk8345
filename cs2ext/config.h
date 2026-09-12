03f59d07ada2d46bc7701db3f974f7d561efbd35
vattmplp-cyber <vattmplp@gmail.com>
2026-09-12 02:07:23 -0500

    Update config.h

--- cs2ext/config.h
+++ cs2ext/config.h
@@ -44,15 +44,6 @@ public:
         write(f, "name_shadow", g_settings.name_shadow);
         write(f, "name_font_size", g_settings.name_font_size);
 
-        // У методі збереження (save):
-        f << "\n[Radar]\n";
-        write(f, "radar_enabled", g_settings.radar.enabled);
-        write(f, "radar_x", g_settings.radar.x);
-        write(f, "radar_y", g_settings.radar.y);
-        write(f, "radar_size", g_settings.radar.size);
-        write(f, "radar_range", g_settings.radar.range);
-        write(f, "radar_aspect_ratio", g_settings.radar.aspect_ratio); // Зберігаємо
-
         f << "\n[HealthText]\n";
         write_arr(f, "hp_text_color", g_settings.hp_text_color, 4);
         write_arr(f, "hp_text_shadow_color", g_settings.hp_text_shadow_color, 4);
@@ -85,6 +76,7 @@ public:
         write(f, "radar_names_font_size", g_settings.radar_names_font_size);
         write(f, "radar_size", g_settings.radar_size);
         write(f, "radar_zoom", g_settings.radar_zoom);
+        write(f, "radar_aspect_ratio", g_settings.radar_aspect_ratio);
         write(f, "radar_bg_alpha", g_settings.radar_bg_alpha);
         write(f, "radar_x", g_settings.radar_x);
         write(f, "radar_y", g_settings.radar_y);
@@ -217,14 +209,6 @@ public:
         read(kv, "draw_head", g_settings.draw_head);
         read(kv, "chams_style", g_settings.chams_style);
         read(kv, "esp_font_index", g_settings.esp_font_index);
-
-        // У методі завантаження (load):
-        read(kv, "radar_enabled", g_settings.radar.enabled);
-        read(kv, "radar_x", g_settings.radar.x);
-        read(kv, "radar_y", g_settings.radar.y);
-        read(kv, "radar_size", g_settings.radar.size);
-        read(kv, "radar_range", g_settings.radar.range);
-        read(kv, "radar_aspect_ratio", g_settings.radar.aspect_ratio); // Завантажуємо
 
         read(kv, "name_position", g_settings.name_position);
         read(kv, "name_offset_x", g_settings.name_offset_x);
