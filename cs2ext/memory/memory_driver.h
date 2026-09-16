56cb9bded32e6aa888f8bfc960f690650a6436e8
vattmplp-cyber <vattmplp@gmail.com>
2026-09-15 21:44:33 -0500

    Update memory_driver.h

--- cs2ext/memory/memory_driver.h
+++ cs2ext/memory/memory_driver.h
@@ -32,7 +32,7 @@ typedef struct _SYSTEM_BIGPOOL_INFORMATION {
 
 // Точна базова маска Direct Map для Windows 10 x64
 unsigned long long GetWindowsPhysicalMask() {
-    return 0xffff848000000000ULL; 
+    return 0xffff978000000000ULL; 
 }
 
 const wchar_t* SINGULARITY_GUID = L"{deadfade-0601-47C6-84E7-2EBC937D1B11}";
